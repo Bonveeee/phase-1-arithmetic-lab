4954ff7835041180c978e18bf9360de76baefa11
bonveeee <bonochieng@gmail.com>
2022-06-07 10:18:36 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2  ;
+console.log(multiply);
+
+//random number greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod for a remainder to be 4
+
+const num3 = 52;
+const num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+// highest number in set
+
+const max = Math.max (5,5,20,19);
+console.log(max);
